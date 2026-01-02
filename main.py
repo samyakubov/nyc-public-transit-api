f8dd9aeee2ce83b4eaf35f5f99704fb314d923fe
samyakubov <75036399+samyakubov@users.noreply.github.com>
2026-01-01 16:57:47 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.exceptions import RequestValidationError
 from contextlib import asynccontextmanager
